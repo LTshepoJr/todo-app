1969ceed4fd257ba83c8e258e1951ce57494175e
Lethabo Kgoele <lethabotshepojr@gmail.com>
2025-02-24 15:15:21 +0200

    added strip method and "|"

--- todo-app.py
+++ todo-app.py
@@ -1,26 +1,28 @@
 countries = []
 cities=[]
 while True:
-    userAction=input("Choose between show, add or exit: ").lower()
+    userAction=input("Choose between show / display, add or exit: ").lower().strip()
     match userAction:
         case 'add':
-            userChooses = input("Choose between country or city: ").lower()
+            userChooses = input("Choose between country or city: ").lower().strip()
             match userChooses:
                 case 'country':
-                    country=input('Enter a country: ')
+                    country=input('Enter a country: ').strip()
                     countries.append(country.title())
                     print('You chose country')
                 case 'city':
-                    city = input('Enter a city: ')
+                    city = input('Enter a city: ').strip()
                     cities.append(city.title())
                     print('You chose city')
-        case 'show':
-            userPlace=input("Show country, city or both?: ").lower()
+        case 'show' | 'display':
+            userPlace=input("Show country, city or both?: ").lower().strip()
             match userPlace:
                 case 'country':
-                    print(countries)
+                    for item in countries:
+                        print(item)
                 case 'city':
-                    print(cities)
+                    for item in cities:
+                        print(item)
                 case 'both':
                     print(countries, cities)
         case 'exit':
