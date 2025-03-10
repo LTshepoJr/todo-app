b1495a9cd3021bccd8aefe326d3b1cd6a48f7d4c
Lethabo Kgoele <lethabotshepojr@gmail.com>
2025-03-11 00:49:47 +0200

    used file opener and file closer to store and retrieve todo app text

--- todo-app.py
+++ todo-app.py
@@ -1,23 +1,26 @@
-todos = []
+fileRead=open('Storage-Texts/todo-app-storage.txt', 'r')
+fileReadLines=fileRead.readlines()
+fileRead.close()
 while True:
     userAction=input("Choose between add, show / display, edit, complete or exit: ").lower().strip()
     match userAction:
         case 'add':
             userAddition=input('Enter a todo: ').strip()
-            todos.append(userAddition.capitalize())
-            fileOverWrite=open('todo-app-storage.txt', 'w')
-            fileOverWrite.writelines(todos)
+            fileReadLines.append(userAddition.capitalize()+'\n')
+            fileOverWrite=open('Storage-Texts/todo-app-storage.txt', 'w')
+            fileOverWrite.writelines(fileReadLines)
+            fileOverWrite.close()
             print('Successfully added!!')
         case 'show' | 'display':
-            for index,todo in enumerate(todos):
+            for index,todo in enumerate(fileReadLines):
                 print(f'{index+1}. {todo.capitalize()}')
-        case 'edit':
+        case 'edit': #crashes
             userEdit=int(input('Enter the number you want edit: '))
             print(f'You are going to change {todos[userEdit - 1]}.')
             userNewTodo=input('What is your new todo?: ').strip()
             todos[userEdit-1]=userNewTodo
             print('Successfully edited!!')
-        case 'complete':
+        case 'complete': #crashes
             popNumber=int(input('Enter the number you completed: '))
             print(f'You completed: {todos.pop(popNumber-1)}')
             print('Successfully removed!!')
