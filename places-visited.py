aad929076c20de09b411f1f5c7f0929546f32190
Lethabo Kgoele <lethabotshepojr@gmail.com>
2025-03-23 13:10:52 +0200

    added text files and a new function to strip line breaks. Only show | display runs, the other match case breaks.

--- places-visited.py
+++ places-visited.py
@@ -1,21 +1,25 @@
-countries = []
-cities=[]
+def readFileText(place):
+    with open(f'Storage-Texts/places-visited-{place}.txt', 'r') as file:
+        fileList=file.readlines()
+        return  fileList
 def userAdditionFunction(userChoose, regionArray):
     region = input(f'Enter a {userChoose}: ').strip()
     return regionArray.append(region.title())
 def userDisplayFunction(regionArray):
     for index, item in enumerate(regionArray):
-        print(f'{index + 1}. {item}')
+        print(f'{index + 1}. {item.strip('\n')}')
 def userEditFunction(number, regionArray, userEdit):
     print(f'You are going to change {regionArray[number - 1]}')
     newCity = input(f'Enter new {userEdit}: ').title()
     regionArray[number - 1] = newCity
 def userVisitFunction(regionArray, number):
     print(f'You have visited: {regionArray.pop(number - 1)}')
+def stripLineBreak(array):
+    return [list.strip('\n') for list in array]
 while True:
     userAction=input("Choose between add, show / display, edit, visited or exit: ").lower().strip()
     match userAction:
-        case 'add':
+        case 'add': #breaks
             userAddition = input("Choose between country or city: ").lower().strip()
             match userAddition:
                 case 'country':
@@ -27,12 +31,12 @@ while True:
             userDisplay=input("Show country, city or both?: ").lower().strip()
             match userDisplay:
                 case 'country':
-                    userDisplayFunction(countries)
+                    userDisplayFunction(readFileText('countries'))
                 case 'city':
-                    userDisplayFunction(cities)
+                    userDisplayFunction(readFileText('cities'))
                 case 'both':
-                    print(f'Countries: {countries}//', f'//Cities: {cities}')
-        case 'edit':
+                    print(f'Countries: {stripLineBreak(readFileText('countries'))}//', f'//Cities: {stripLineBreak(readFileText('cities'))}')
+        case 'edit': #breaks
             userEdit=input('Edit country or city?: ').lower().strip()
             number=int(input('Enter a number: '))
             match userEdit:
@@ -41,7 +45,7 @@ while True:
                 case 'country':
                     userEditFunction(number, countries, userEdit)
             print('Successfully edited!!')
-        case 'visited':
+        case 'visited': #breaks
             userVisit=input('Country or city visited?: ').lower().strip()
             number=int(input('Enter a number: '))
             match userVisit:
