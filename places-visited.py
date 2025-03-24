72a93e827b647084b174fe45793f09ab08dea9ab
Lethabo Kgoele <lethabotshepojr@gmail.com>
2025-03-24 12:45:05 +0200

    created write file function

--- places-visited.py
+++ places-visited.py
@@ -1,31 +1,38 @@
 def readFileText(place):
     with open(f'Storage-Texts/places-visited-{place}.txt', 'r') as file:
         fileList=file.readlines()
-        return  fileList
+        return fileList
+def writeFileText(user, regionArray):
+    with open(f'Storage-Texts/places-visited-'
+              f'{'countries'if user=='country' else 'cities'}.txt', 'w') as file:
+        file.writelines(regionArray)
 def userAdditionFunction(userChoose, regionArray):
     region = input(f'Enter a {userChoose}: ').strip()
-    return regionArray.append(region.title())
+    regionArray.append(region.title()+'\n')
+    writeFileText(userChoose, regionArray)
+def userEditFunction(number, regionArray, userEdit):
+    print(f'You are going to change {regionArray[number - 1].strip('\n')}')
+    newPlace = input(f'Enter new {userEdit}: ').title()
+    regionArray[number - 1] = newPlace+'\n'
+    writeFileText(userEdit, regionArray)
+def userVisitFunction(regionArray, number, userVisit):
+    print(f'You have visited: {regionArray.pop(number - 1).strip('\n')}')
+    writeFileText(userVisit, regionArray)
 def userDisplayFunction(regionArray):
     for index, item in enumerate(regionArray):
         print(f'{index + 1}. {item.strip('\n')}')
-def userEditFunction(number, regionArray, userEdit):
-    print(f'You are going to change {regionArray[number - 1]}')
-    newCity = input(f'Enter new {userEdit}: ').title()
-    regionArray[number - 1] = newCity
-def userVisitFunction(regionArray, number):
-    print(f'You have visited: {regionArray.pop(number - 1)}')
 def stripLineBreak(array):
     return [list.strip('\n') for list in array]
 while True:
     userAction=input("Choose between add, show / display, edit, visited or exit: ").lower().strip()
     match userAction:
-        case 'add': #breaks
+        case 'add':
             userAddition = input("Choose between country or city: ").lower().strip()
             match userAddition:
                 case 'country':
-                    userAdditionFunction(userAddition, countries)
+                    userAdditionFunction(userAddition, readFileText('countries'))
                 case 'city':
-                    userAdditionFunction(userAddition, cities)
+                    userAdditionFunction(userAddition, readFileText('cities'))
             print('Successfully added!!')
         case 'show' | 'display':
             userDisplay=input("Show country, city or both?: ").lower().strip()
@@ -35,24 +42,25 @@ while True:
                 case 'city':
                     userDisplayFunction(readFileText('cities'))
                 case 'both':
-                    print(f'Countries: {stripLineBreak(readFileText('countries'))}//', f'//Cities: {stripLineBreak(readFileText('cities'))}')
-        case 'edit': #breaks
+                    print(f'Countries: {stripLineBreak(readFileText('countries'))}//',
+                          f'//Cities: {stripLineBreak(readFileText('cities'))}')
+        case 'edit':
             userEdit=input('Edit country or city?: ').lower().strip()
             number=int(input('Enter a number: '))
             match userEdit:
                 case 'city':
-                    userEditFunction(number, cities, userEdit)
+                    userEditFunction(number, readFileText('cities'), userEdit)
                 case 'country':
-                    userEditFunction(number, countries, userEdit)
+                    userEditFunction(number, readFileText('countries'), userEdit)
             print('Successfully edited!!')
-        case 'visited': #breaks
+        case 'visited':
             userVisit=input('Country or city visited?: ').lower().strip()
             number=int(input('Enter a number: '))
             match userVisit:
                 case 'city':
-                    userVisitFunction(cities, number)
+                    userVisitFunction(readFileText('cities'), number, userVisit)
                 case 'country':
-                    userVisitFunction(countries, number)
+                    userVisitFunction(readFileText('countries'), number, userVisit)
             print('Successfully removed!!')
         case 'exit':
             break
