68e38f25e83ed0f01f220aa294d7781d7d316fd9
Lethabo Kgoele <lethabotshepojr@gmail.com>
2025-03-28 20:49:50 +0200

    added if, elif, else statement for better user usage of the programme

--- places-visited.py
+++ places-visited.py
@@ -6,7 +6,11 @@ def readFileText(place):
 
 def writeFileText(user, regionArray):
     with open('Storage-Texts/places-visited-'
-              f'{'countries' if user=='country' else 'cities'}.txt',
+              f'{'countries' 
+              if user=='country'
+              else 'cities'
+              if user=='city'
+              else ''}.txt',
               'w') as file:
         file.writelines(regionArray)
 
@@ -34,63 +38,108 @@ def stripLineBreak(array):
 
 while True:
     
-    userAction=input('Choose between add, show / display, edit, visited or exit: ').lower().strip()
+    userAction=input(
+'''Choose between add, show / display, edit, visited or exit.
+(You can include city or country): ''').lower().strip()
     
-    match userAction:
-        case 'add':
-            userAddition = input('Choose between country or city: ').lower().strip()
-            match userAddition:
-                case 'country':
-                    userAdditionFunction(readFileText('countries'),
-                                         userAddition)
-                    print('Successfully added!!')
-                case 'city':
-                    userAdditionFunction(readFileText('cities'),
-                                         userAddition)
-                    print('Successfully added!!')
-
-        case 'edit':
-            userEdit=input('Edit country or city?: ').lower().strip()
-            number=int(input('Enter a number: '))
-            match userEdit:
-                case 'city':
-                    userEditFunction(readFileText('cities'),
-                                     userEdit,
-                                     number)
-                    print('Successfully edited!!')
-                case 'country':
-                    userEditFunction(readFileText('countries'),
-                                     userEdit,
-                                     number)
-                    print('Successfully edited!!')
-            
-        case 'visited':
-            userVisit=input('Country or city visited?: ').lower().strip()
-            number=int(input('Enter a number: '))
-            match userVisit:
-                case 'city':
-                    userVisitFunction(readFileText('cities'),
-                                      userVisit,
-                                      number)
-                    print('Successfully removed!!')
-                case 'country':
-                    userVisitFunction(readFileText('countries'),
-                                      userVisit,
-                                      number)
-                    print('Successfully removed!!')
-
-        case 'show' | 'display':
-            userDisplay=input('Show country, city or both?: ').lower().strip()
-            match userDisplay:
-                case 'country':
-                    userDisplayFunction(readFileText('countries'))
-                case 'city':
-                    userDisplayFunction(readFileText('cities'))
-                case 'both':
+    if ' ' in userAction:
+        userChooses=userAction.split()[0].lower()
+        userPlace=userAction.split()[1].lower()
+        if userPlace=='country' or 'cities':
+            if userChooses=='add':
+                userAdditionFunction(readFileText(f'countries' 
+                                                    if userPlace=='country'
+                                                    else 'cities' 
+                                                    if userPlace=='city' 
+                                                    else ''),
+                                                userPlace)
+                print('Successfully added!!')
+            elif userChooses=='edit':
+                number=int(input('Enter a number: '))
+                userEditFunction(readFileText(f'countries' 
+                                                    if userPlace=='country'
+                                                    else 'cities' 
+                                                    if userPlace=='city' 
+                                                    else ''),
+                                        userPlace,
+                                        number)
+                print('Successfully edited!!')
+            elif userChooses=='visited':
+                number=int(input('Enter a number: '))
+                userVisitFunction(readFileText(f'countries' 
+                                                    if userPlace=='country'
+                                                    else 'cities' 
+                                                    if userPlace=='city' 
+                                                    else ''),
+                                        userPlace,
+                                        number)
+                print('Successfully removed!!')
+            elif userChooses=='show' or 'display':
+                if userAction.split()[1]!='both':
+                    userDisplayFunction(readFileText(f'countries' 
+                                                    if userPlace=='country'
+                                                    else 'cities' 
+                                                    if userPlace=='city' 
+                                                    else ''))
+                else:
                     print(f'Countries: {stripLineBreak(readFileText('countries'))}//',
-                          f'//Cities: {stripLineBreak(readFileText('cities'))}')
-            
-        case 'exit':
-            break
+                            f'//Cities: {stripLineBreak(readFileText('cities'))}')
+    else:
+        match userAction:
+            case 'add':
+                userAddition = input('Choose between country or city: ').lower().strip()
+                match userAddition:
+                    case 'country':
+                        userAdditionFunction(readFileText('countries'),
+                                            userAddition)
+                        print('Successfully added!!')
+                    case 'city':
+                        userAdditionFunction(readFileText('cities'),
+                                            userAddition)
+                        print('Successfully added!!')
+
+            case 'edit':
+                userEdit=input('Edit country or city?: ').lower().strip()
+                number=int(input('Enter a number: '))
+                match userEdit:
+                    case 'city':
+                        userEditFunction(readFileText('cities'),
+                                        userEdit,
+                                        number)
+                        print('Successfully edited!!')
+                    case 'country':
+                        userEditFunction(readFileText('countries'),
+                                        userEdit,
+                                        number)
+                        print('Successfully edited!!')
+                
+            case 'visited':
+                userVisit=input('Country or city visited?: ').lower().strip()
+                number=int(input('Enter a number: '))
+                match userVisit:
+                    case 'city':
+                        userVisitFunction(readFileText('cities'),
+                                        userVisit,
+                                        number)
+                        print('Successfully removed!!')
+                    case 'country':
+                        userVisitFunction(readFileText('countries'),
+                                        userVisit,
+                                        number)
+                        print('Successfully removed!!')
+
+            case 'show' | 'display':
+                userDisplay=input('Show country, city or both?: ').lower().strip()
+                match userDisplay:
+                    case 'country':
+                        userDisplayFunction(readFileText('countries'))
+                    case 'city':
+                        userDisplayFunction(readFileText('cities'))
+                    case 'both':
+                        print(f'Countries: {stripLineBreak(readFileText('countries'))}//',
+                            f'//Cities: {stripLineBreak(readFileText('cities'))}')
+                
+            case 'exit':
+                break
 
 print('Bye!')
